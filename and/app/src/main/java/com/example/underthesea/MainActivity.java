624ac727ad5ad0b7b046dd33f27329ee1963392c
hyerim <vhzkfl2451@naver.com>
2019-05-31 15:04:43 +0900

    메인 버튼 추가 액티비티 추가

--- and/app/src/main/java/com/example/underthesea/MainActivity.java
+++ and/app/src/main/java/com/example/underthesea/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.underthesea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onCameraClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAlbumClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), AlbumActivity.class);
+        startActivity(intent);
+    }
+
+    public void onHistoryClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
+        startActivity(intent);
+    }
+
 }
