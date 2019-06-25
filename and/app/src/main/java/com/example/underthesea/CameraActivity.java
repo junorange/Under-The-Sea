3a44fcd73efb832da8bd00151d4c4c7d89c3ffe1
hyerim <vhzkfl2451@naver.com>
2019-06-26 04:03:14 +0900

    permission add

--- and/app/src/main/java/com/example/underthesea/CameraActivity.java
+++ and/app/src/main/java/com/example/underthesea/CameraActivity.java
@@ -1,17 +1,38 @@
 package com.example.underthesea;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CameraActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
+        checkP();
+
+    }
+
+    private void checkP(){
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+
+
+        if(permissionCheck== PackageManager.PERMISSION_DENIED){
+            Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent2);
+        } else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(intent);
+        }
     }
 
 }
+
